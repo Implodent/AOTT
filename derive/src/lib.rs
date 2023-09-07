1579b5ef5e6e222fea8af24d7217f03d9b5555a4
nothendev <borodinov.ilya@gmail.com>
2023-09-07 17:34:01 +0300

    fix extras

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -4,16 +4,18 @@
 //! WARNING: unformatted shitcode ahead.
 //! fix PRs accepted.
 
+use std::str::FromStr;
+
 use proc_macro::TokenStream as TS;
 use proc_macro2::{Ident, Span, TokenStream};
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
         parse::{Parse, Parser},
         punctuated::Punctuated,
         token::Comma,
-        AngleBracketedGenericArguments, Expr, ExprPath, FnArg, GenericArgument, GenericParam,
-        ItemFn, Lifetime, LifetimeParam, Meta, MetaNameValue, Path, PathArguments, PathSegment,
-        ReturnType, Type, TypePath, TypeReference,
+        AngleBracketedGenericArguments, Expr, ExprLit, ExprPath, FnArg, GenericArgument,
+        GenericParam, ItemFn, Lifetime, LifetimeParam, Lit, LitStr, Meta, MetaNameValue, Path,
+        PathArguments, PathSegment, ReturnType, Type, TypePath, TypeReference,
 };
 
 // example usage:
@@ -53,7 +55,19 @@ fn parser_impl(args: TokenStream, ts: TokenStream) -> Result<TokenStream, syn::E
                         path,
                         value: Expr::Path(ExprPath { path: ext, .. }),
                         ..
-                }) if path.is_ident(&Ident::new("extras", Span::call_site())) => Some(ext.clone()),
+                }) if path.is_ident(&Ident::new("extras", Span::call_site())) => {
+                        Some(ext.to_token_stream().to_string())
+                }
+                Meta::NameValue(MetaNameValue {
+                        path,
+                        value:
+                                Expr::Lit(ExprLit {
+                                        lit: Lit::Str(lit), ..
+                                }),
+                        ..
+                }) if path.is_ident(&Ident::new("extras", Span::call_site())) => {
+                        Some(lit.value().to_owned())
+                }
                 _ => None,
         });
         let mut lifetimes = vec![];
@@ -74,13 +88,19 @@ fn parser_impl(args: TokenStream, ts: TokenStream) -> Result<TokenStream, syn::E
                                         }
                                         t => t,
                                 };
-                                let extras = extras.unwrap_or_else(|| {
-                                        Path::parse.parse2(quote!(extra::Err<#ty>)).unwrap()
-                                });
+                                let extras = extras
+                                        .map(|t| {
+                                                Type::parse
+                                                        .parse2(TokenStream::from_str(&t).unwrap())
+                                                        .unwrap()
+                                        })
+                                        .unwrap_or_else(|| {
+                                                Type::parse.parse2(quote!(extra::Err<#ty>)).unwrap()
+                                        });
                                 let ty_for_ret = ty.clone();
                                 let extras_for_ret = extras.clone();
                                 let output = f.sig.output;
-                                f.sig.output = ReturnType::Type(Default::default(), Box::new(Type::Path(TypePath { qself: None, path: Path {leading_colon: None, segments: core::iter::once(PathSegment {ident: Ident::new("PResult", Span::call_site()), arguments: PathArguments::AngleBracketed(AngleBracketedGenericArguments { colon2_token: None, gt_token: Default::default(), lt_token: Default::default(), args: Punctuated::from_iter([GenericArgument::Type(ty_for_ret), GenericArgument::Type(match output { ReturnType::Default => Type::Tuple(syn::TypeTuple { paren_token: Default::default(), elems: Punctuated::new() }), ReturnType::Type(_, t) => map_ty(lifetime, *t) }), GenericArgument::Type(Type::Path(TypePath { qself: None, path: extras_for_ret }))]) })}).collect()} })));
+                                f.sig.output = ReturnType::Type(Default::default(), Box::new(Type::Path(TypePath { qself: None, path: Path {leading_colon: None, segments: core::iter::once(PathSegment {ident: Ident::new("PResult", Span::call_site()), arguments: PathArguments::AngleBracketed(AngleBracketedGenericArguments { colon2_token: None, gt_token: Default::default(), lt_token: Default::default(), args: Punctuated::from_iter([GenericArgument::Type(ty_for_ret), GenericArgument::Type(match output { ReturnType::Default => Type::Tuple(syn::TypeTuple { paren_token: Default::default(), elems: Punctuated::new() }), ReturnType::Type(_, t) => map_ty(lifetime, *t) }), GenericArgument::Type(extras_for_ret)]) })}).collect()} })));
 
                                 pat.ty = Box::new(Type::Reference(TypeReference {
                                         elem: Box::new(Type::Path(TypePath {
